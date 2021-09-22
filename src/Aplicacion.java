a45a087ca162385b80f12cd96a757bdb14181e6e
Angel-Mosqueda <66578497+Angel-Mosqueda@users.noreply.github.com>
2021-09-22 18:30:50 -0500

    Add files via upload

---
+++ src/Aplicacion.java
@@ -0,0 +1,20 @@
+import Accion.Accion;
+import Accion.Factory;
+
+public class Aplicacion {
+    
+    public static void main(String[] args){
+//        Accion a1 = Factory.construir(tipo: "Colega");
+//        Accion a2 = Factory.construir(tipo: "Profesor");
+//        
+//        a1.saludar();
+//        a1.despedirse(); 
+//        System.out.println(a1.tipo());
+//        
+//        a2.saludar();
+//        a2.despedirse();        
+//        System.out.println(a2.tipo());
+            Accion a1 = Factory.algo();
+            a1.despedirse();
+    }
+}
